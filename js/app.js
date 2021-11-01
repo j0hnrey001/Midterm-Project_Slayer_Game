004f3e83e9437b5b35d4c2bdeeeaf050f357f406
johnreyalan@gmail.com <johnreyalan001@gmail.com>
2021-11-01 08:49:47 +0700

    Adding heal player method, to heal the player in the game

--- js/app.js
+++ js/app.js
@@ -6,26 +6,48 @@ new Vue({
 
     data: {
       monsterHealth: 100,
-      playerHealth: 100
+      playerHealth: 100,
+      currentRound: 0
     },
     computed: {
-      monsterBarStyles() {
+      monsterBarStyle() {
         return {width: this.monsterHealth + '%'};
       },
-      playerBarStyles() {
+      playerBarStyle() {
         return {width: this.playerHealth + '%'};
+      },
+      specialAttackRound () {
+        return this.currentRound % 5 !==0
       }
     },
     methods: {
       attackMonster() {
-        const attackValue = getRandomValue (10, 20);
+        this.currentRound ++
+        const attackValue = getRandomValue (8, 13);
         this.monsterHealth -= attackValue;
         this.attackPlayer()
       },
       attackPlayer () {
-        const attackValue =getRandomValue (15, 30);
+        const attackValue =getRandomValue (10, 17);
         this.playerHealth -= attackValue;
       },
+      specialAttackMonster(){
+        this.currentRound ++
+        const attackValue = getRandomValue (11, 23);
+        this.monsterHealth -= attackValue;
+        this.attackPlayer()
+      },
+      healPlayer() {
+        this.currentRound ++;
+        const healValue = getRandomValue(10, 22);
+        if(this.playerHealth + healValue > 100 ) {
+          this.playerHealth = 100;
+        }
+        else{
+        this.playerHealth += healValue;
+        }
+        this.attackPlayer()
+      }
     }
   })
 
